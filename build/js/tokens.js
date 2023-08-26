abcf0e1ec22594673ae814388dc9a4ef93b66f85
Maurício Caminha <mauricio.caminha@hotmail.com>
2023-08-26 18:30:28 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 26 Aug 2023 20:33:52 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#09090b",
+  "ColorPurple500": "#8b5cf6"
+};
