8cd841f7b0439addab98e1a481139f1895c781af
Mauricio-Caminha <Mauricio-Caminha@users.noreply.github.com>
2023-08-26 23:04:34 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,17 @@
 /**
  * Do not edit directly
- * Generated on Sat, 26 Aug 2023 20:33:52 GMT
+ * Generated on Sat, 26 Aug 2023 23:04:34 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
   "ColorGray800": "#1c1c1f",
   "ColorGray900": "#09090b",
+  "ColorPurple400": "#9b74f3",
   "ColorPurple500": "#8b5cf6"
 };
